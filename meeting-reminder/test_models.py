321520f2e10002f19afd633ea991fa8dfe0f0ede
leslie291 <lesles078@gmail.com>
2026-04-04 13:02:29 -0700

    Update comments in test_models.py for clarity

--- meeting-reminder/test_models.py
+++ meeting-reminder/test_models.py
@@ -3,10 +3,10 @@
 from meeting.models import Meeting
 
 m1 = Meeting("4/4/2026", "Lou, Liwen, Henry", "6035 clubhouse Ln", "417-849-3518")
-print(m1.is_canceled())  # 應該印 False
+print(m1.is_canceled())  # should print False
 
 m2 = Meeting("3/14/2026", "Meeting canceled", "No address", "")
-print(m2.is_canceled())  # 應該印 True
+print(m2.is_canceled())  # should print True
 
 m3 = Meeting("5/16/2026", "", "", "")
-print(m3.is_canceled())  # 應該印 True
+print(m3.is_canceled())  # should print True
